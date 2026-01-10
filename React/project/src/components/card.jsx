297ac0d0c18c863f818cab8efe13e88765e4ac73
vijeth44-pro <captainmncaptain@gmail.com>
2026-01-10 10:42:13 +0530

    Refactor code structure for improved readability and maintainability

--- React/project/src/components/card.jsx
+++ React/project/src/components/card.jsx
@@ -1,14 +1,24 @@
-import React from 'react'
+import React from "react";
+import Berserk from "../assets/berserk.jpg";
 
-export default function() {
+export default function () {
+    const newStyle= {backgroundColor: "blue",
+          width: "300px",
+          height: "300px",
+          color: "black",
+          border:"2px solid red"}
   return (
     <div>
-        {/* first bracket for jsx and second for style object */}
-        <div style={{backgroundColor:'blue', width:'300px', height:'300px' ,color:'black', borderRadius:'10px'}}>
-            <img src="" alt="" />
-            <h1>Title</h1>
-            <h6>Category</h6>
-        </div>
+      {/* first bracket for jsx and second for style object */}
+      <div
+        style={
+          newStyle
+        }
+      >
+        <img src={Berserk} alt="berserk" style={{width:'300px' , height:'300px' }}/>
+        <h1>Title</h1>
+        <h6>Category</h6>
+      </div>
     </div>
-  )
+  );
 }
