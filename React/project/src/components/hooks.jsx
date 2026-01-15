e2e07392c7046af0c3e06af0b4b30f74641c19d3
vijeth44-pro <captainmncaptain@gmail.com>
2026-01-15 10:15:00 +0530

    chore: add comments to hooks component for better understanding of useState

--- React/project/src/components/hooks.jsx
+++ React/project/src/components/hooks.jsx
@@ -4,12 +4,11 @@ import {useState} from 'react'
 export default function hooks() {
     //use state
     //use effect
+
     //1.use state
+    //const [state, setstate] = useState(any value)
+    //state-> variable , setstate-> function which we use to change value of state
     
-
-
-
-
   return (
     <div>hooks</div>
   )
