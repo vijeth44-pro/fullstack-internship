0a2109068e8ae7b343713f370cea5f8f4e404455
vijeth44-pro <captainmncaptain@gmail.com>
2026-01-15 11:13:01 +0530

    feat: enhance hooks component with dynamic background and text change functionality

--- React/project/src/components/hooks.jsx
+++ React/project/src/components/hooks.jsx
@@ -8,8 +8,36 @@ export default function hooks() {
     //1.use state
     //const [state, setstate] = useState(any value)
     //state-> variable , setstate-> function which we use to change value of state
-    
+
+
+    const[name,setName] = useState("hello")
+    const[bg,setBg] = useState(false)
+    const[user,setUser] = useState({
+        name:"John",
+        age:30,
+        email:"john@gmail.com"
+    })
+
+//turnary operator
+// condition ? true : false
+
+
+//logic operator
+// && -> (if condition is true then only execute the code after &&)
+// || -> (if condition is false then only execute the code after ||)
+
   return (
-    <div>hooks</div>
+    <div style={{
+        backgroundColor: bg ? 'black' : 'white',
+        color: bg ? 'white' : 'black'
+    }
+    }>
+        <p>{name}</p>
+        <button onClick={()=>setName("World")}>Change Text</button>
+        <button onClick={()=>setBg(!bg)}>Change Bg</button>
+        <p>{user.name}</p>
+        <p>{user.age}</p>
+        <p>{user.email}</p>
+    </div>
   )
 }
