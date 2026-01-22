ca972dcdad215749df170f94435bd82e49eb9581
vijeth44-pro <captainmncaptain@gmail.com>
2026-01-22 10:44:33 +0530

    feat: enhance form functionality with user input handling and submission

--- React/project/src/pages/form.jsx
+++ React/project/src/pages/form.jsx
@@ -1,22 +1,58 @@
-import React from 'react'
-import { useState } from 'react'  
+import React from "react";
+import { useState } from "react";
 
 //rafce
 //form
 //input form with name and place and submit button
 export default function form() {
-    const [userData,setUserData] = useState({
-        name:'',
-        place:''
-    });
-    
+  const [userData, setUserData] = useState({
+    name: "", //object
+    place: ""
+  });
+  
+  const [allData,setAllData] = useState([])
+
+  const handleSubmit = () => {
+    console.log("User Data:", userData);
+    setAllData([...allData, userData])
+    setUserData({name:"",place:""})
+    alert("Form submitted successfully!")
+  };
+
   return (
     <div>
-        <input type="text" placeholder='Enter your name'/>
-        <br />
-        <input type="text" placeholder='Enter your place'/>
-        <br />
-        <button>Submit</button>
+      <input
+        type="text"
+        placeholder="Enter your name"
+        onChange={(e) => setUserData({ ...userData, name: e.target.value })}
+        value={userData.name}
+      />
+      <br />
+      <input
+        type="text"
+        placeholder="Enter your place"
+        onChange={(e) => setUserData({ ...userData, place: e.target.value })}
+        value={userData.place}
+      />
+      <br />
+      <button onClick={handleSubmit}>Submit</button>
+      <div>
+        <table border={{}}>
+          <tr>
+            <th>SL NO</th>
+            <th>Name</th>
+            <th>Place</th>
+          </tr>
+          {allData.map(()=> (
+            
+          ))}
+          <tr>
+            <td>1</td>
+            <td>John</td>
+            <td>London</td>
+          </tr>
+        </table>
+      </div>
     </div>
-  )
+  );
 }
