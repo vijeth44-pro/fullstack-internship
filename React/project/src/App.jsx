afdc2c00d60f9f2e3d868e001f5987540dbc7836
vijeth44-pro <captainmncaptain@gmail.com>
2026-01-23 04:56:52 +0530

    feat: implement form handling and data display in TaskCard component

--- React/project/src/App.jsx
+++ React/project/src/App.jsx
@@ -19,7 +19,7 @@ function App() {
 
   return (
     <div>
-      <Form />
+      {/* <Form /> */}
       <TaskCard />
     </div>
   )
--- React/project/src/pages/taskcard.jsx
+++ React/project/src/pages/taskcard.jsx
@@ -1,11 +1,58 @@
-import React from 'react'
+import React from "react";
+import { useState } from "react";
 
-function taskcard() {
+//rafce
+//form
+//input form with name and place and submit button
+export default function taskcard() {
+  const [userData, setUserData] = useState({
+    name: "", //object
+    place: ""
+  });
+  
+  const [allData,setAllData] = useState([])
+
+  const handleSubmit = () => {
+    console.log("User Data:", userData);
+    setAllData([...allData, userData])
+    setUserData({name:"",place:""})
+    alert("Form submitted successfully!")
+  };
+
   return (
     <div>
-      
+      <input
+        type="text"
+        placeholder="Enter your name"
+        onChange={(e) => setUserData({ ...userData, name: e.target.value })}
+        value={userData.name}
+      />
+      <br />
+      <input
+        type="text"
+        placeholder="Enter your place"
+        onChange={(e) => setUserData({ ...userData, place: e.target.value })} //to extend the object
+        value={userData.place}
+      />
+      {/* important input values are onchange and value */}
+      <br />
+      <button onClick={handleSubmit}>Submit</button>
+      <div>
+        <table border={{}}>
+          <tr>
+            <th>SL NO</th>
+            <th>Name</th>
+            <th>Place</th>
+          </tr>
+          {allData.map((item, index)=> (
+            <tr>
+            <td>{index+1}</td>
+            <td>{item.name}</td>
+            <td>{item.place}</td>
+          </tr>
+          ))}
+        </table>
+      </div>
     </div>
-  )
+  );
 }
-
-export default task-card
