313052041a9cccf515dbf03db00c3dced82f42d2
vijeth44-pro <captainmncaptain@gmail.com>
2026-01-24 05:38:27 +0530

    fix: update Muicard component for improved layout and correct price display

--- React/project/src/components/Muicard.jsx
+++ React/project/src/components/Muicard.jsx
@@ -1,25 +1,30 @@
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
+import Box from "@mui/material/Box";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
 
 export default function BasicCard({ name, price, category, description }) {
   return (
-    <Card sx={{ minWidth: 275 }}>
+    <Card sx={{ minWidth: 275 , maxWidth: 400}}>
       <CardContent>
-        <Typography gutterBottom sx={{ color: 'text.secondary', fontSize: 18 }}>
+        <Typography gutterBottom sx={{ color: "text.secondary", fontSize: 18 }}>
           {name}
         </Typography>
-        <Typography variant="h5" component="div">
-          Price: ₹{price}
+
+        <Typography variant="h6" component="div">
+          Price: Rs{price}
         </Typography>
-        <Typography variant="body2">
+
+        <Typography sx={{ color: "text.secondary", mb: 1.5 }}>
           {category}
         </Typography>
-         <Typography variant="P" component="div">
-            {description}
+
+        <Typography variant="body2">
+          {description}
         </Typography>
+        
       </CardContent>
       <CardActions>
         <Button size="small">Learn More</Button>
